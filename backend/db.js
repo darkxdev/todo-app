ab255b747018db6d2472e2617ab131a69cd4cf93
DarkxDev <bryanfernandohurtado@gmail.com>
2023-05-25 12:32:22 -0500

    Updated DB_LINK

--- backend/db.js
+++ backend/db.js
@@ -2,9 +2,6 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // Create a new instance of Sequelize and configure it
-const sequelize = new Sequelize(process.env.DB_NAME || 'todo_app_dev', process.env.DB_USERNAME || 'postgres', process.env.DB_PASSWORD || 'database', {
-  host: process.env.DB_HOST || 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(process.env.DB_LINK);
 
 module.exports = sequelize;
